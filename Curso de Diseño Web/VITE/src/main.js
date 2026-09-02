3638e08a876b517bce277a82a884e8ad0952bfd1
Gabriel <luisgabrielhernandezresendiz@gmail.com>
2026-09-02 16:37:01 -0600

    Sección de vite completa.
    * Importar archivos de CSS.
    * Como compilar para producción.

--- Curso de Diseño Web/VITE/src/main.js
+++ Curso de Diseño Web/VITE/src/main.js
@@ -1,12 +1,16 @@
+import './assets/estilos.css';
+
 import imagen from './assets/bosque.png';
 import cargarContador from './contador';
+import clases from './assets/imagen.module.css';
+
 
 console.log('Hola mundo');
 
 document.querySelector('#app').innerHTML = `
   <h1>Hola Gabriel!</h1>
-  <img src="${imagen}" alt="Imagen de un bosque" id="imagen" />
-  <h2>Veces clickeado: <span id="cuenta">0</span></h2>
+  <img src="${imagen}" alt="Imagen de un bosque" id="imagen" class="${clases.imagen}" />
+  <h2>Veces clickeado: <span id="cuenta" class="${clases.contador}">0</span></h2>
 `;
 
 cargarContador();

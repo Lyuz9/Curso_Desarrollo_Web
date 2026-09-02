415dd1adce4e7981cbf252aecc463ed5db64bb39
Gabriel <luisgabrielhernandezresendiz@gmail.com>
2026-09-01 19:24:52 -0600

    Introduccion a Vite
    * Importando imagenes.
    * Módulos de ES6.

--- Curso de Diseño Web/VITE/src/main.js
+++ Curso de Diseño Web/VITE/src/main.js
@@ -1,5 +1,12 @@
+import imagen from './assets/bosque.png';
+import cargarContador from './contador';
+
 console.log('Hola mundo');
 
 document.querySelector('#app').innerHTML = `
   <h1>Hola Gabriel!</h1>
+  <img src="${imagen}" alt="Imagen de un bosque" id="imagen" />
+  <h2>Veces clickeado: <span id="cuenta">0</span></h2>
 `;
+
+cargarContador();
